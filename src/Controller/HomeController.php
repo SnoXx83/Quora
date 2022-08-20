87c99d016294f36e38f3dedaf6d4b1585c417236
Maxime Larquetoux <maximelarquetoux@gmail.com>
2022-08-20 12:03:51 +0200

    Affichage de la liste des question

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,8 +11,32 @@ class HomeController extends AbstractController
     #[Route('/', name: 'home')]
     public function index(): Response
     {
+        $questions = [
+            [
+                'title' => 'je suis une question',
+                'content' => "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Atque ex minus voluptates illo perspiciatis voluptatibus, odio quisquam perferendis sapiente, error fugiat eveniet t",
+                'rating' => -12,
+                'author' => [
+                    'name' => "maxime larquetoux",
+                    'avatar' => "https://randomuser.me/api/portraits/men/49.jpg"
+                ],
+                'nbResponse' => 15
+            ],
+            [
+                'title' => 'je suis une question',
+                'content' => "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Atque ex minus voluptates illo perspiciatis voluptatibus, odio quisquam perferendis sapiente, error fugiat eveniet t",
+                'rating' => 15,
+                'author' => [
+                    'name' => "Rachid Jeffaly",
+                    'avatar' => "https://randomuser.me/api/portraits/men/50.jpg"
+                ],
+                'nbResponse' => 12
+            ]
+        ];
+
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'questions' => $questions
         ]);
     }
 }
