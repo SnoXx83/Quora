8451af3057f6287f9d7f701f4b7903bb9b16e0b5
Maxime Larquetoux <maximelarquetoux@gmail.com>
2023-05-13 12:55:08 +0200

    Creation de l'entite Question, plus la page detaillee d'une question

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\QuestionRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,30 +10,10 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'home')]
-    public function index(): Response
+    public function index(QuestionRepository $questionRepository): Response
     {
-        $questions =[
-            [
-                'title'=> 'je suis une question',
-                'content' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloribus qui rem odio veritatis voluptatibus! Accusamus quaerat voluptatum repellat sunt, magni libero aut laboriosam at id totam dolore deserunt rerum. Excepturi?',
-                'rating' =>20,
-                'author'=> [
-                'name'=> 'Maxime Larquetoux',
-                'avatar' =>"https://randomuser.me/api/portraits/men/77.jpg"
-                ],
-                'nbResponse'=> 15
-            ],
-            [
-                'title'=> 'je suis une question',
-                'content' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloribus qui rem odio veritatis voluptatibus! Accusamus quaerat voluptatum repellat sunt, magni libero aut laboriosam at id totam dolore deserunt rerum. Excepturi?',
-                'rating' => -8,
-                'author'=> [
-                'name'=> 'Alice DesFlandres',
-                'avatar' =>"https://randomuser.me/api/portraits/women/68.jpg"
-                ],
-                'nbResponse'=> 10
-            ]
-        ];
+
+        $questions= $questionRepository->findAll();
 
         return $this->render('home/index.html.twig', [
             'questions' => $questions,
