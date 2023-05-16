82f9a449e33dfed91abe2059b08a90387cbbdab6
Maxime Larquetoux <maximelarquetoux@gmail.com>
2023-05-16 16:19:43 +0200

    Creation de l'entite user

--- src/Entity/Comment.php
+++ src/Entity/Comment.php
@@ -33,6 +33,10 @@ class Comment
     #[ORM\JoinColumn(nullable: false)]
     private ?Question $question = null;
 
+    #[ORM\ManyToOne(inversedBy: 'comments')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?User $author = null;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -82,6 +86,18 @@ class Comment
     public function setQuestion(?Question $question): self
     {
         $this->question = $question;
+
+        return $this;
+    }
+
+    public function getAuthor(): ?User
+    {
+        return $this->author;
+    }
+
+    public function setAuthor(?User $author): self
+    {
+        $this->author = $author;
 
         return $this;
     }
