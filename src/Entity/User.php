888ef35870d61210c202097132c80303827401e8
Maxime Larquetoux <maximelarquetoux@gmail.com>
2023-05-16 17:34:12 +0200

    Mise en place du formulaire d'inscription et de regles de validation

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -8,6 +8,7 @@ use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
@@ -18,6 +19,8 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     private ?int $id = null;
 
     #[ORM\Column(length: 180, unique: true)]
+    #[Assert\Email(message: "Veuillez renseigner une adresse mail valide.")]
+    #[Assert\NotBlank(message:"Veuillez renseigner une adresse mail.")]
     private ?string $email = null;
 
     #[ORM\Column]
@@ -27,12 +30,16 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
      * @var string The hashed password
      */
     #[ORM\Column]
+    #[Assert\Length(min: 6 , minMessage:"Le mot de passe doit faire au minimum 6 caractères.")]
+    #[Assert\NotBlank(message: "Veuillez entrer un mot de passe.")]
     private ?string $password = null;
 
     #[ORM\Column(length: 255)]
+    #[Assert\NotBlank(message: "Veuillez renseigner votre prenom.")]
     private ?string $firstname = null;
 
     #[ORM\Column(length: 255)]
+    #[Assert\NotBlank(message: "Veuillez renseigner votre nom.")]
     private ?string $lastname = null;
 
     #[ORM\OneToMany(mappedBy: 'author', targetEntity: Question::class, orphanRemoval: true)]
@@ -42,6 +49,8 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     private Collection $comments;
 
     #[ORM\Column(length: 255)]
+    #[Assert\Url(message: "Votre image doit provenir d'une URL.")]
+    #[Assert\NotBlank(message: "Veuillez renseigner une image.")]
     private ?string $picture = null;
 
     public function __construct()
